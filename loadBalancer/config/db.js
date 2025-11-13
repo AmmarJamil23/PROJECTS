22ad79fc7765f0e00f17cb9d040e7e9e2e32481d
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-11-13 17:14:19 +0500

    initial code

---
+++ loadBalancer/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+async function connectDB() {
+    try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/mernLB");
+    console.log("MongoDB Connected");
+    } catch (error){
+        console.error("MongoDB Error:", error.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
---
+++ loadBalancer/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/api/health", (req, res) => {
+    res.json( {msg : "API running"});
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
