9abcae1f05b62564ff0d51162391b4ce7cce03c6
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-12-05 18:57:16 +0500

    frontend structure and libraries installed

---
+++ smartclient/client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground p-6'>
+      <h1 className='text-3xl font-bold'>
+        Dashboard will be displayed here
+        </h1>
+
+      <p className='text-muted-foreground mt-2'>
+        Here the SmartClient Frontend will be displayed
+        </p>
+    </div>
+  )
+}
+
+export default App
