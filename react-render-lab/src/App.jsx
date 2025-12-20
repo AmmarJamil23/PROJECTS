011360fbf0541977c414c2c15fea873841f2c7a9
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-12-20 10:23:01 +0500

    initial react render

---
+++ react-render-lab/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='p-5'>
+      <h1 className='text-4xl'>React Render Lab</h1>
+      <p>All the renders in the console tab</p>
+      </div>
+  )
+}
+
+export default App
