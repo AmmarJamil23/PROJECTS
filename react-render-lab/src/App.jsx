dfc6b2670930e3eccb07e04adeb2314e0ddd6806
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-12-24 22:11:51 +0500

    added states rendering logic rules

--- react-render-lab/src/App.jsx
+++ react-render-lab/src/App.jsx
@@ -8,6 +8,21 @@ const App = () => {
 
   const [count, setCount] = useState(0);
 
+  const doubleCount = count * 2;
+
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Learn React", done: false },
+    { id: 2, title: "Practice hooks", done: true},
+    { id: 3, title: "Build project", done: false}
+  ]);
+
+  const [showDone, setShowDone] = useState(false);
+
+  const visibleTasks = showDone ? tasks.filter((t) => t.done) : tasks;
+
+
+
+
 
   const handleClick = useCallback(() => {
     console.log("Clicked");
@@ -37,16 +52,61 @@ const App = () => {
       <h1 className='text-2xl font-bold'>React Render Lab</h1>
 
       <button className='px-4 py-2 bg-green-400 rounded font-bold'
-      onClick={() => setCount(count + 1)}
+      onClick={() => {
+        setCount(count + 1);
+
+      }}
       >
         Increment 
       </button>
 
+      <button
+      className='mt-2 px-4 py-2 bg-orange-600 rounded'
+      onClick={() => setCount(count + 1)}
+      >
+        Increment Count Only
+      </button>
+
       <p className='mt-2'>Count value: {count}</p>
+
+      <p>Double Count: {doubleCount}</p>
 
       <RenderCounter label="App Component" />
 
       <Child onClick={handleClick} />
+
+      <ul className='mt-2 space-y-2'>
+        {visibleTasks.map((task) => (
+          <li key={task.id}>
+            {task.title} {task.done ? "(done)" : ""}
+
+          </li>
+        ))}
+      </ul>
+
+      <button className='mt-4 px-4 py-2 bg-blue-600 rounded'
+      onClick={() => {
+        setShowDone(!showDone);
+      }}
+      >
+        Toggle Done Tasks
+      </button>
+
+    <button
+        className='mt-2 px-4 py-2 bg-green-400 rounded'
+        onClick={() => {
+          // 1. Add the new task as usual
+          setTasks([
+            ...tasks,
+            { id: Date.now(), title: "New Task", done: false },
+          ]);
+
+         
+          setShowDone(false);
+        }}
+      >
+        Add Task
+      </button>
       
       </div>
   )
