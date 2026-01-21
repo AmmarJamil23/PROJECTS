2e4faf82823b0ae68f652c089c8b98f718e78e90
AmmarJamil23 <ammarjamil7000@gmail.com>
2026-01-21 22:03:04 +0500

    typescript errors

--- talk-to-your-pdf/server/src/controllers/upload.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-const uploadPdf = async (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "Upload endpoint ready"
-  });
-};
-
-module.exports = { uploadPdf };
--- talk-to-your-pdf/server/src/routes/upload.route.ts
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { uploadPdf } = require("../controllers/upload.controller");
-
-const router = express.Router();
-
-router.post("/", uploadPdf);
-
-module.exports = router;
