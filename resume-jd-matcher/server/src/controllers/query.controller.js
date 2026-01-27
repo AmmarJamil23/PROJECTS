53b4c2bfbe802fd540f1ec6bb8116463a0acc9e0
AmmarJamil23 <ammarjamil7000@gmail.com>
2026-01-27 19:05:07 +0500

    llm services started

---
+++ resume-jd-matcher/server/src/controllers/query.controller.js
@@ -0,0 +1,3 @@
+import { embeddings } from "../services/embedding.service.js"
+import { retrieveRelevantChunks } from "../services/retrieval.service.js"
+import { llm } from "../services/llm.service.js"
