1d0ac7d18b48d9d83065ef2bdf230baa96097c26
AmmarJamil23 <ammarjamil7000@gmail.com>
2026-02-21 10:19:35 +0500

    Remove OpsAI and multi-doc-rag folders

--- multi-doc-rag/server/apps/core/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-class DocumentUploadSerializer(serializers.Serializer):
-    file = serializers.FileField()
-    document_name = serializers.CharField(max_length=255)
-
-class ResumeUploadSerializer(serializers.Serializer):
-    resume = serializers.FileField()
-    candidate_name = serializers.CharField(max_length=255)
